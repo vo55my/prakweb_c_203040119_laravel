cf9fd0c09746e2b511fdc168bb313ad8ccdcd208
Dhaifullah Hilmy <dhaifullahhilmy@gmail.com>
2022-10-20 20:56:40 +0700

    Video 3 - Struktur Folder, Routes & View

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Dhaifullah Hilmy",
+        "email" => "dhaifullahhilmy@gmail.com",
+        "image" => "hilmy.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
