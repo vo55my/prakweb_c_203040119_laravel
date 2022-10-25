e8a29f088dab767faae41d4371b1baf5839291b9
Dhaifullah Hilmy <dhaifullahhilmy@gmail.com>
2022-10-25 15:43:43 +0700

    Video 8 - Post Category & Eloquent Relationship

--- laravel/resources/views/post.blade.php
+++ laravel/resources/views/post.blade.php
@@ -3,7 +3,7 @@
 @section('container')
     <article>
       <h2>{{ $post->title }}</h2>
-      <h5>{{ $post->author }}</h5>
+      <h5>By. Dhaifullah Hilmy <a href="/categories/{{ $post->category->slug }}"> {{ $post->category->name }}</a></h5>
       {!! $post->body !!}
     </article>
 
