697f2b2bd1e8010298b386539d97156b08601ddc
Dhaifullah Hilmy <dhaifullahhilmy@gmail.com>
2022-10-31 23:32:44 +0700

    Video 13 - Searching & Pagination

--- laravel/resources/views/post.blade.php
+++ laravel/resources/views/post.blade.php
@@ -5,12 +5,12 @@
     <div class="row justify-content-center mb-5">
       <div class="col-md-8">
         <h2 class="mb-3">{{ $post->title }}</h2>
-        <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none"> {{ $post->category->name }}</a></p>
+        <p>By. <a href="/posts?authors={{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/posts?category={{ $post->category->slug }}" class="text-decoration-none"> {{ $post->category->name }}</a></p>
         <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid">
         <article class="my-3 fs-5">
           {!! $post->body !!}
         </article>
-        <a href="/blog" class="text-decoration-none d-block mt-3">Back To Posts</a>
+        <a href="/posts" class="text-decoration-none d-block mt-3">Back To Posts</a>
       </div>
     </div>
   </div>
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -19,20 +19,22 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home', [
-        "title" => "Home"
+        "title" => "Home",
+        "active" => "home"
     ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
         "title" => "About",
+        "active" => "about",
         "name" => "Dhaifullah Hilmy",
         "email" => "dhaifullahhilmy@gmail.com",
         "image" => "hilmy.jpg"
     ]);
 });
 
-Route::get('/blog', [PostController::class, 'index']);
+Route::get('/posts', [PostController::class, 'index']);
 
 // Halaman single post
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
@@ -42,20 +44,5 @@ Route::get('/categories', function () {
         'title' => 'Post Categories',
         "active" => "categories",
         'categories' => Category::all(),
-    ]);
-});
-
-Route::get('/categories/{category:slug}', function (Category $category) {
-    return view('posts', [
-        'title' => "Post By Category : $category->name",
-        "active" => "categories",
-        'posts' => $category->posts->load('category', 'author')
-    ]);
-});
-
-Route::get('/authors/{author:username}', function (User $author) {
-    return view('posts', [
-        'title' => "Post By Author : $author->name",
-        'posts' => $author->posts->load('category', 'author')
     ]);
 });
