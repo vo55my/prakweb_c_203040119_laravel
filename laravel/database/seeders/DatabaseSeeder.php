5ea0de7a6f65c6f05f87c066fff68b2b6a54cac4
Dhaifullah Hilmy <dhaifullahhilmy@gmail.com>
2022-11-24 17:09:40 +0700

    Video 21 - Upload Image

--- laravel/database/seeders/DatabaseSeeder.php
+++ laravel/database/seeders/DatabaseSeeder.php
@@ -17,11 +17,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
-        // User::create([
-        //     'name' => 'Dhaifullah Hilmy',
-        //     'email' => 'dhaifullahhilmy@gmail.com',
-        //     'password' => bcrypt('12345')
-        // ]);
+        User::create([
+            'name' => 'Dhaifullah Hilmy',
+            'username' => 'dhaifullahhilmy',
+            'email' => 'dhaifullahhilmy@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
 
         // User::create([
         //     'name' => 'Mick Jagger',
