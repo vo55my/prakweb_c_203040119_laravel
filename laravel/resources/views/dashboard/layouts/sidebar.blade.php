4e7efbc19da0010ffd84c46c1e1a74b900429b76
Dhaifullah Hilmy <dhaifullahhilmy@gmail.com>
2022-11-26 14:13:29 +0700

    Video 23 - Authorization

--- laravel/resources/views/dashboard/layouts/sidebar.blade.php
+++ laravel/resources/views/dashboard/layouts/sidebar.blade.php
@@ -10,9 +10,23 @@
       <li class="nav-item">
         <a class="nav-link {{ Request::is('dashboard/posts*') ? 'active' : '' }}" href="/dashboard/posts">
           <span data-feather="file-text" class="align-text-bottom"></span>
-          My Post
+          My Posts
         </a>
       </li>
     </ul>
+
+    @can('admin')
+      <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1">
+        <span>Administrator</span>
+      </h6>
+      <ul class="nav flex-column">
+        <li class="nav-item">
+          <a class="nav-link {{ Request::is('dashboard/categories*') ? 'active' : '' }}" href="/dashboard/categories">
+            <span data-feather="grid" class="align-text-bottom"></span>
+            Post Categories
+          </a>
+        </li>
+      </ul>
+    @endcan
   </div>
 </nav>
